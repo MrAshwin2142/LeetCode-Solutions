2a1d29dd830d077040df31baa81a53a2768b7ffc
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2023-09-12 08:50:29 +0530

    Time: 14 ms (34.66%), Space: 13.1 MB (56.74%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int, vector<int>> mp;
-        int n=groupSizes.size();
-        for(int i=0; i<n; i++){
-            int x=groupSizes[i];
-            mp[x].push_back(i);
+    vector<vector<int>> groupThePeople(vector<int>& g) {
+        map<int,vector<int>> mp;
+        int n=g.size();
+        for(int i=0;i<n;i++){
+            mp[g[i]].push_back(i);
         }
         vector<vector<int>> ans;
-        for(auto& [x, v]: mp){
-            int q=v.size()/x;
-            for(int i=0; i<q; i++){
-                vector<int> v_sz(x);
-                copy(v.cbegin()+(i*x), v.cbegin()+((i+1)*x), v_sz.begin());
-                ans.push_back(v_sz);
+        for(auto i:mp){
+            if(i.first==i.second.size()) ans.push_back(i.second);
+            else{int t=i.second.size()/i.first;
+            vector<int> v(i.first);
+            int k=0;
+            for(int j=0;j<i.second.size();j++){
+                v[k++]=i.second[j];
+                if(k==i.first){
+                    ans.push_back(v);
+                    k=0;
+                }
+            }
+            // ans.push_back(v);
             }
         }
         return ans;
