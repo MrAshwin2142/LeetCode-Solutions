028e63131968b0f153375b91414eb1e669223ba7
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-03-04 21:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (73.70%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-   bool special(vector<vector<int>> &mat, int row,int col){
-    int m=mat.size();
-    int n=mat[0].size();
-    int r=0;
-    int c=0;
-
-    for(int i=0;i<n;i++){
-        if(mat[row][i]==1){
-            r++;
-        }
-    }
-    for(int i=0;i<m;i++){
-        if(mat[i][col]==1){
-            c++;
-        }
-    }
-
-    if(r==1 && c==1){
-        return true;
-    }
-    return false;
-
-
-
-   }
     int numSpecial(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        
-        int c=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==1){
-                    if(special(mat,i,j)){
-                        c++;
-                    }
-                }
+        int special = 0;
+
+        for (const auto& row : mat) {
+            if (accumulate(row.begin(), row.end(), 0) == 1) {
+                int colIdx = distance(row.begin(), find(row.begin(), row.end(), 1));
+                special += getColumnSum(mat, colIdx) == 1;
             }
-        } 
-        return c;       
+        }
+
+        return special;        
+    }
+
+private:
+    int getColumnSum(const vector<vector<int>>& mat, int colIdx) {
+        int sum = 0;
+        for (const auto& row : mat) {
+            sum += row[colIdx];
+        }
+        return sum;
     }
 };
