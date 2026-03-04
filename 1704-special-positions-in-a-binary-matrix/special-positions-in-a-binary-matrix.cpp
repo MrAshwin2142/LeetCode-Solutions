68d118988f357278cd5621a6a2bf0d5062218bec
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-03-04 21:43:29 +0530

    Time: 7 ms (5.37%) | Memory: 16.5 MB (73.70%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
+   bool special(vector<vector<int>> &mat, int row,int col){
+    int m=mat.size();
+    int n=mat[0].size();
+    int r=0;
+    int c=0;
+
+    for(int i=0;i<n;i++){
+        if(mat[row][i]==1){
+            r++;
+        }
+    }
+    for(int i=0;i<m;i++){
+        if(mat[i][col]==1){
+            c++;
+        }
+    }
+
+    if(r==1 && c==1){
+        return true;
+    }
+    return false;
+
+
+
+   }
     int numSpecial(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<bool> row(n,true),col(m,true);
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=0;j<m;j++){
-                sum+=mat[i][j];
+        int m=mat.size();
+        int n=mat[0].size();
+        
+        int c=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    if(special(mat,i,j)){
+                        c++;
+                    }
+                }
             }
-            if(sum!=1) row[i]=false;
-        }
-        for(int j=0;j<m;j++){
-            int sum=0;
-            for(int i=0;i<n;i++){
-                sum+=mat[i][j];
-                cout<<sum<<endl;
-            }
-            if(sum!=1) col[j]=false;
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1 && row[i] && col[j]) ans++;
-            }
-        }
-        return ans;
+        } 
+        return c;       
     }
 };
