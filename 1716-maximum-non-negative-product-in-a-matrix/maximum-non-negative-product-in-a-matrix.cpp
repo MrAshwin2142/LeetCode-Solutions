904251fc217cb8bee7c4d97a08664512c1abc77d
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-03-26 01:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (22.27%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,24 +1,29 @@
-int64_t dp[15][15][2];
 class Solution {
 public:
-    static int maxProductPath(vector<vector<int>>& grid) {
-        const int r=grid.size(), c=grid[0].size(), MOD=1e9+7;
-        int64_t p=dp[0][0][0]=dp[0][0][1]=grid[0][0];
-        for(int j=1; j<c; j++){
-            p*=grid[0][j];
-            dp[0][j][0]=dp[0][j][1]=p;
+    typedef long long ll;
+    int mod=1e9+7;
+    int maxProductPath(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<ll>>dp_max(n,vector<ll>(m,0));
+        vector<vector<ll>>dp_min(n,vector<ll>(m,0));
+        dp_max[0][0]=dp_min[0][0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            dp_min[i][0]=dp_max[i][0]=dp_max[i-1][0]*grid[i][0];
         }
-        p=grid[0][0];
-        for(int i=1; i<r; i++){
-            p*=grid[i][0];
-            dp[i][0][0]=dp[i][0][1]=p;
-            for(int j=1; j<c; j++){
-                int x=grid[i][j];
-                auto [minP, maxP]=minmax({x*dp[i][j-1][0], x*dp[i][j-1][1], x*dp[i-1][j][0], x*dp[i-1][j][1]});
-                dp[i][j][0]=minP, dp[i][j][1]=maxP;
+        for(int i=1;i<m;i++){
+            dp_min[0][i]=dp_max[0][i]=dp_max[0][i-1]*grid[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                ll a=dp_min[i-1][j]*grid[i][j];
+                ll b=dp_max[i-1][j]*grid[i][j];
+                ll c=dp_min[i][j-1]*grid[i][j];
+                ll d=dp_max[i][j-1]*grid[i][j];
+                dp_min[i][j]=min({a,b,c,d});
+                dp_max[i][j]=max({a,b,c,d});
             }
         }
-        int64_t ans=dp[r-1][c-1][1];
-        return ans<0?-1: ans%MOD;
+        if(dp_max[n-1][m-1]<0)return -1;
+        return dp_max[n-1][m-1]%mod;
     }
 };
