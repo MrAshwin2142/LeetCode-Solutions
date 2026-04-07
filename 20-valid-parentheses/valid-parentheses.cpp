61bc981186815d321dedfc72754e1307de933979
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-04-07 22:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c:s){
+            if(c=='[' || c=='(' || c=='{') stk.push(c);
+            else{
+                if(stk.empty()) return false;
+                if(c==']' && stk.top()=='[') stk.pop();
+               else if(c==')' && stk.top()=='(') stk.pop();
+                else if(c=='}' && stk.top()=='{') stk.pop();
+                else stk.push(c);
+            }
+        }
+        return stk.empty();
+    }
+};
