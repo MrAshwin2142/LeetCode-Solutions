ba84101007f65b510bf0240a0400484f7b2e11fc
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-08-19 22:08:28 +0530

    Time: 95 ms (19.59%) | Memory: 79.8 MB (27.70%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -7,8 +7,9 @@ public:
             mp[i[0]].push_back(i[1]);
         }
         int ans = 2*(n-mp.size());
-        vector<bool> v(11,true);
+        
         for(auto i:mp){
+            vector<bool> v(10,true);
             int t=0;
             for(int j:i.second) v[j]=false;
             int p1 = (v[2] & v[3] & v[4] & v[5]);
@@ -16,10 +17,7 @@ public:
             int p3 = (v[6] & v[7] & v[8] & v[9]);
             if( p1 && p3) t=2;
             else if(p1 || p2 || p3) t=1;
-            cout<<t<<endl;
             ans+=t;
-            for(int i=0;i<11;i++){cout<<v[i]<<" "; v[i]=true;}
-            cout<<endl;
         }
         return ans;
     }
