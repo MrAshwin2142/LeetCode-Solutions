fc218986fb8f7586a03784342e362e17562327c9
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2026-08-25 17:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (53.82%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        set<int> s;
-        for(int i:nums) s.insert(i);
-        for(int i=1;i<100;i++){
-            int t=k*i;
-            if(s.find(t)==s.end()) return t;
+        vector<int> freq(201,false);
+        for(int i:nums) freq[i]=true;
+        for(int i=1;i*k<=200;i++){
+            if(!freq[i*k]) return i*k;
         }
         return 101;
     }
